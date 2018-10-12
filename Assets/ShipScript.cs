5767713284933fc58894ed7d77882d0a08f53e55
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2018-10-12 15:34:33 +0200

    Finishing touches

--- Assets/ShipScript.cs
+++ Assets/ShipScript.cs
@@ -5,146 +5,155 @@ using UnityEngine;
 public class ShipScript : MonoBehaviour
 {
 
-    //TODO: få screen warpingen att fungera
 
+    // Variabler:
 
-
-    //variabler:
-
-    //skeppets färger
+    // Skeppets färger
     public SpriteRenderer rend;
     public Color leftGreen;
     public Color rightBlue;
     public Color newColor;
 
-    //random färger
+    // Random färger
     public float red;
     public float green;
     public float blue;
-    public Color spaceColor;
+    public Color randomColor;
     public SpriteRenderer shipRend;
 
 
+    // Skeppets rörlighet
+    public float rotationSpeedLeft = 150f; // Bestämmer värdet på variabeln som sedan blir hur snabbt man svänger vänster
+    public float rotationSpeedRight = 300f; // Bestämmer värdet på variabeln som sedan blir hur snabbt man svänger höger
+    public float shipSpeed; // En variabel som jag använder för att få en speed på mitt skepp 
 
-    //skeppets rörlighet
-    [Range(-720, 720)]
-    public float rotationSpeedLeft = 150f;
 
-    [Range(-720, 720)]
-    public float rotationSpeedRight = 300f;
-    public float shipSpeed;
+    public float timer; // Skapar en variabel till timern
+    public int timerCount; // Skapar en variabel till timern som ska 
 
-    //timer
-    public float timer;
-    public int timerCount;
-
-    //screen wrapping
+    // Screen wrapping
     public Vector3 newPosition;
 
-    //random spawn
+    // Random spawn
     public float ySpawn;
     public float xSpawn;
+    public float randomSpawn;
+
+
+
+
 
 
     // Use this for initialization
     void Start()
     {
-        //randomiserar skeppets hastighet
-        shipSpeed = Random.Range(2f, 8f);
 
-        //SKA randomiserar skeppets x axel spawn
-        Vector3 randomSpawn;
-            xSpawn = Random.Range(-8.35f, 8.35f);
+        shipSpeed = Random.Range(2f, 8f); // Randomiserar skeppets hastighet varje gång man startar spelet
 
-        //SKA randomiserar skeppets y axel spawn
-            ySpawn = Random.Range(-4.5f, 4.5f);
+
+        Vector2 randomSpawn; // Gör randomSpawn variabeln till en Vector med 2 dimensioner
+
+        xSpawn = Random.Range(-8.3f, 8.3f); // Randomiserar värdet på variabeln xSpawn varje gång man startar spelet
+
+
+        ySpawn = Random.Range(-4.25f, 4f); // Randomiserar värdet på variabeln ySpawn varje gång man startar spelet
+                                           //^jag har ändrat 4.25f till 4f eftersom det var ett bugg att man spawnade för högt upp och fastade in en konstant screenwarps
+
+        randomSpawn = new Vector2(xSpawn, ySpawn); // Gör randomSpawn variabeln till en Vector med x och y kordinater
+
+        transform.position = randomSpawn; // Att positionen av skeppet sätts till en random spawn
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //gör så att skeppet svänger vänster och byter färg till grön på knapptryck A
+        // Gör så att skeppet svänger vänster och byter färg till grön på knapptryck A
         if (Input.GetKey(KeyCode.A))
         {
+            
+            transform.Rotate(0f, 0f, rotationSpeedLeft * Time.deltaTime); // Gör så att skeppet svänger vänster
 
-            //styrning
-            transform.Rotate(0f, 0f, rotationSpeedLeft * Time.deltaTime);
-
-            //färg
-            rend.color = newColor;
-            rend.color = leftGreen;
+            rend.color = leftGreen; // Renderar den gröna färgen på skeppet
 
         }
 
-        //gör så att skeppet svänger höger och byter färg till blå på knapptryck D
+        // Gör så att skeppet svänger höger och byter färg till blå på knapptryck D
         if (Input.GetKey(KeyCode.D))
         {
 
-            //styrning
-            transform.Rotate(0f, 0f, -rotationSpeedRight * Time.deltaTime);
+            transform.Rotate(0f, 0f, -rotationSpeedRight * Time.deltaTime); // Gör så att skeppet svänger höger
 
-            //färg
-            rend.color = newColor;
-            rend.color = rightBlue;
+            rend.color = rightBlue; // Renderar den blåa färgen på skeppet
 
         }
 
 
-        //gör så att skeppet åker framåt automatiskt
-        transform.Translate(shipSpeed * Time.deltaTime, 0f, 0f, Space.Self);
+
+        transform.Translate(shipSpeed * Time.deltaTime, 0f, 0f, Space.Self); // gör så att skeppet åker framåt automatiskt
 
 
-        //gör så att skeppets fart halveras med knapptryck
+        // Gör så att skeppets fart halveras om man trycker på knapptryck S
         if (Input.GetKey(KeyCode.S))
         {
 
-            transform.Translate(-(shipSpeed / 2f) * Time.deltaTime, 0f, 0f, Space.Self);
+            transform.Translate(-(shipSpeed / 2f) * Time.deltaTime, 0f, 0f, Space.Self); // tar speeden och halverar den
 
         }
 
 
 
-        //timer
+        // Timer
 
-        timer += 1 * Time.deltaTime;
+        timer += 1 * Time.deltaTime; // Räknar tid per frame
 
-        if (timer > timerCount)
+        if (timer > timerCount) // Om timer är större än timerCount händer det som står i måsvingarna
         {
 
-            timerCount += 1; //lägger till 1 varje sekund
+            timerCount += 1; // Lägger till +1 varje sekund
 
-            print(timerCount); //printar sekunderna
+            print(timerCount); // Printar sekunderna
 
         }
 
 
-        //screen wrapping (verkar inte fungera)
-        Vector3 newPosition = transform.position;
-
-        if (newPosition.x > 5.5f || newPosition.x < -5.5f)
-        {
-            newPosition.x -= newPosition.x;
-        }
-
-        if (newPosition.y > 9.5f || newPosition.y < -9.5f)
-        {
-            newPosition.y -= newPosition.y;
-        }
-
-        transform.position = newPosition;
 
 
 
         //random färg på space
-        if (Input.GetKeyDown(KeyCode.Space)) //läser av om space trycks ned
+        if (Input.GetKeyDown(KeyCode.Space)) // Om "Space" trycks ned så händer det som står inom måsvingarna nedan
         {
-            red = Random.Range(0f, 1f); //randomiserar värdet på röd färg
-            green = Random.Range(0f, 1f); //randomiserar värdet på grön färg
-            blue = Random.Range(0f, 1f); //randomiserar värdet på blå färg
 
-            spaceColor = new Color(red, green, blue);
-            shipRend.color = spaceColor;
+            red = Random.Range(0f, 1f); // Randomiserar värdet på röd färg
+            green = Random.Range(0f, 1f); // Randomiserar värdet på grön färg
+            blue = Random.Range(0f, 1f); // Randomiserar värdet på blå färg
+
+            randomColor = new Color(red, green, blue); // Skapar en ny färg med de randomiserade RGB färgerna
+            shipRend.color = randomColor; // Renderar den nya färgen på skeppet
+
         }
+
+
+        //screen warping
+        Vector2 newPosition = transform.position;
+
+        if (newPosition.x > 8.5f || newPosition.x < -8.5f) // Gör så att när man når någon av de här kordinaterna på x axeln så händer det som står inom måsvingarna
+        {
+
+            newPosition.x -= newPosition.x * 2; // Säger till skeppet att flytta sig till kordinaterna mittemot där man åker in i "väggen" (x axeln)
+
+        }
+
+        if (newPosition.y > 4.5f || newPosition.y < -4.5f) // Gör så att när man når någon av de här kordinaterna på y axeln så händer det som står inom måsvingarna
+        {
+
+            newPosition.y -= newPosition.y * 2; // Säger till skeppet att flytta sig till kordinaterna mittemot där man åker in i "väggen" (y axeln)
+
+        }
+
+        transform.position = newPosition; // Att positionen av skeppet sätts till en ny position
+
     }
+
 }
